d573de58ebf5e867ff4044157175f9cb7e39dcf9
Caio Tarifa <caiotarifa@gmail.com>
2021-04-19 02:56:19 -0300

    Removing Error404 page

--- template/src/router/routes.js
+++ template/src/router/routes.js
@@ -11,12 +11,6 @@ const routes = [
         component: () => import('pages/Index.vue')
       }
     ]
-  },
-
-  // TODO: This should come from asteroid
-  {
-    path: '*',
-    component: () => import('pages/Error404.vue')
   }
 ]
 
